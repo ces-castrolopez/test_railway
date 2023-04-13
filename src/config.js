a7cc3eb66016ddda8835dfdeb1d1cdf4f97300a5
Cesar Castro <ces.castrolopez@gmail.com>
2023-04-12 22:24:19 -0600

    FirstCommit

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export const PORT = process.env.PORT || 3000
+export const DB_HOST = process.env.DB_HOST || 'containers-us-west-160.railway.app'
+export const DB_USER = process.env.DB_USER || 'root'
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'wRSaBgeYZbfTEqE0nLSg'
+export const DB_NAME = process.env.DB_NAME || 'railway'
+export const DB_PORT = process.env.DB_PORT || 5892

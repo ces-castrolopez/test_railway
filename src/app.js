0c1278d18ee554436893de57ebd4e546b4eff9f7
Cesar Castro <ces.castrolopez@gmail.com>
2023-04-13 12:29:48 -0600

    Changes

--- src/app.js
+++ src/app.js
@@ -1,15 +1,14 @@
 import express from 'express'
-import { pool } from './db.js'
+import {pool} from './db.js'
 import {PORT} from './config.js'
 
 const app = express()
 
 app.get('/api', async (req, res) => {
-  console.log(req.query.studentId);
   const [rows] = await pool.query('SELECT * FROM students WHERE id='+req.query.studentId+';') 
-  res.json(rows[0]);
-
+  res.json(rows[0]);  
+  console.log("Given data: " +JSON.stringify(rows[0]));
 })
 
-app.listen(PORT)
+app.listen(PORT);
 console.log('Server on port', PORT)
